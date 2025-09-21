80b3dd42c3d5c6bb08c1ad114b1aed371090adef
Cody Partington <codythatsme@gmail.com>
2025-09-21 16:35:56 +1000

    chages

--- apps/web/src/components/ui/timeline.tsx
+++ apps/web/src/components/ui/timeline.tsx
@@ -141,7 +141,7 @@ export const Timeline = ({
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-background flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
+              <h3 className="hidden text-primary md:block text-xl md:pl-20 md:text-5xl font-bold">
                 {item.title}
               </h3>
               {index === activeIndex ? (
@@ -175,6 +175,7 @@ export const Timeline = ({
             style={{
               height: heightTransform,
               opacity: opacityTransform,
+              y: -20, // nudge up so the progress meets the top of the marker
             }}
             className="absolute inset-x-0 top-0 w-[2px] bg-gradient-to-t from-primary via-primary/70 to-transparent from-[0%] via-[10%] rounded-full"
           />
